eb59a1dea032c3709d1439e085dc48cf69208f4f
MirrorwindAbyss <abdumalikunusov06@gmail.com>
2023-08-21 11:49:22 +0500

    Done

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,17 +1,16 @@
 package com.epam.demo;
 
+import com.epam.utils.StringUtils;
+
 import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        //magic happens here
-        public static boolean isPositiveNumber(String str) {
-            for (String arg : args) {
-                if (!StringUtils.isPositiveNumber(arg)) {
-                    return false;
-                }
+        for (String arg : args) {
+            if (!StringUtils.isPositiveNumber(arg)) {
+                return false;
             }
-            return true;
         }
+        return true;
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,16 +1,11 @@
 package com.epam.utils;
 
 public class StringUtils {
-            try
-
-    {
-        return Double.parseDouble(str) > 0;
-    } catch(NumberFormatException |
-    NullPointerException e)
-
-    {
-        return false;
+    public static boolean isPositiveNumber(String str) {
+        try {
+            return Double.parseDouble(str) > 0;
+        } catch (NumberFormatException | NullPointerException e) {
+            return false;
+        }
     }
 }
-                }
-                        }
